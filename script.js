3c24536059d7b4a7d91bfd932753958eb60dee71
AlmadelX <almadelx@proton.me>
2023-07-23 19:12:10 +0500

    Use class syntax

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 function toggleForm() {
-  showFormButton.classList.toggle("hidden");
-  form.classList.toggle("hidden");
+  showFormButton.classList.toggle('hidden');
+  form.classList.toggle('hidden');
 }
 
 function updateLibrary() {
   const data = getFormData();
-  libraryContent.push(new Book(data.title, data.author, data.country, data.language, data.genre, data.pages, data.read));
+  libraryContent.push(
+    new Book(
+      data.title,
+      data.author,
+      data.country,
+      data.language,
+      data.genre,
+      data.pages,
+      data.read
+    )
+  );
   displayLibrary();
 }
 
@@ -15,16 +25,15 @@ function getFormData() {
   toggleForm();
 
   const result = {};
-  formData.forEach((value, key) => result[key] = value);
+  formData.forEach((value, key) => (result[key] = value));
   return result;
 }
 
 function displayLibrary() {
-  library.textContent = "";
+  library.textContent = '';
   for (let i = 0; i < libraryContent.length; ++i) {
     const book = libraryContent[i];
-    library.innerHTML += 
-      `<div class="card" data-index="${i}">
+    library.innerHTML += `<div class="card" data-index="${i}">
       <div class="card-info">
         <h2>${book.title}</h2>
         <div class="card-content">
@@ -39,26 +48,32 @@ function displayLibrary() {
           <p>Pages:</p>
           <p>${book.pages}</p>
           <p>Read:</p>
-          <p>${book.read ? "Yes" : "No"}</p>
+          <p>${book.read ? 'Yes' : 'No'}</p>
         </div>
       </div>
       <div class="card-buttons">
-        <button class="card-button-read">${book.read ? "Haven't read" : "Have read"}</button>
+        <button class="card-button-read">${
+          book.read ? "Haven't read" : 'Have read'
+        }</button>
         <button class="card-button-remove">Remove</button>
       </div>`;
   }
 
   // Add button event listeners
-  const readButtons = library.querySelectorAll(".card-button-read");
-  const removeButtons = library.querySelectorAll(".card-button-remove");
-  
-  readButtons.forEach(button => button.addEventListener("click", () => toggleReadStatus(button)));
-  removeButtons.forEach(button => button.addEventListener("click", () => removeBook(button)));
+  const readButtons = library.querySelectorAll('.card-button-read');
+  const removeButtons = library.querySelectorAll('.card-button-remove');
+
+  readButtons.forEach(button =>
+    button.addEventListener('click', () => toggleReadStatus(button))
+  );
+  removeButtons.forEach(button =>
+    button.addEventListener('click', () => removeBook(button))
+  );
 }
 
 function toggleReadStatus(button) {
   const card = button.parentNode.parentNode;
-  const bookIndex = card.getAttribute("data-index");
+  const bookIndex = card.getAttribute('data-index');
 
   libraryContent[bookIndex].toggleReadStatus();
   displayLibrary();
@@ -66,37 +81,39 @@ function toggleReadStatus(button) {
 
 function removeBook(button) {
   const card = button.parentNode.parentNode;
-  const bookIndex = card.getAttribute("data-index");
+  const bookIndex = card.getAttribute('data-index');
 
   libraryContent.splice(bookIndex, 1);
   displayLibrary();
 }
 
-function Book(title, author, country, language, genre, pages) {
-  this.title = title;
-  this.author = author;
-  this.country = country;
-  this.language = language;
-  this.genre = genre;
-  this.pages = pages;
-  this.read = false;
+class Book {
+  constructor(title, author, country, language, genre, pages) {
+    this.title = title;
+    this.author = author;
+    this.country = country;
+    this.language = language;
+    this.genre = genre;
+    this.pages = pages;
+    this.read = false;
+  }
+
+  toggleReadStatus() {
+    this.read = !this.read;
+  }
 }
 
-Book.prototype.toggleReadStatus = function() {
-  this.read = !this.read;
-}
-
-const showFormButton = document.querySelector("#show-form");
-const form = document.querySelector(".sidebar-form");
-const library = document.querySelector(".library");
-const readButtons = document.querySelectorAll(".card-button-read");
+const showFormButton = document.querySelector('#show-form');
+const form = document.querySelector('.sidebar-form');
+const library = document.querySelector('.library');
+const readButtons = document.querySelectorAll('.card-button-read');
 
 let libraryContent = [];
 
 // Setup listeners
-showFormButton.addEventListener("click", toggleForm);
+showFormButton.addEventListener('click', toggleForm);
 
-form.addEventListener("submit", e => {
+form.addEventListener('submit', e => {
   e.preventDefault();
   updateLibrary();
 });
